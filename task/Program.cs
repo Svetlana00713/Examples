82ebdc40bb42fee8e82321ad408367166d282274
Svetlana <start61@mail.ru>
2023-02-10 20:51:38 +0300

    Added Task

---
+++ task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = 0;
+square = number * number;
+Console.Write($"Квадрат числа {number} = {square}");
+
